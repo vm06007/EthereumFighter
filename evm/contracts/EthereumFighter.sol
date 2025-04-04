144c93146cf2308ae05a3576cfdf6a6564a41723
emanherawy <eman.herawy@gmaail.com>
2025-04-04 18:03:27 +0200

    add token sorting

--- evm/contracts/EthereumFighter.sol
+++ evm/contracts/EthereumFighter.sol
@@ -99,5 +99,12 @@ function withdraw() public {
     // if the game is not started, we need to revert the transaction
     // TODO    
 }
-    
+    // coppied from unswap library
+        // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+    }
+
 }
