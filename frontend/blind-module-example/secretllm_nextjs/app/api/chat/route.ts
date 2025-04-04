909aae73030bb802097d022dc3323bcb83418bc6
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-04-04 23:15:08 +0800

    💬 cleanup add chat components
    - new packages:  react-markdown && remark-gfm

--- frontend/blind-module-example/secretllm_nextjs/app/api/chat/route.ts
+++ frontend/blind-module-example/secretllm_nextjs/app/api/chat/route.ts
@@ -5,13 +5,11 @@ export async function POST(req: Request) {
         const body = await req.json();
         console.log(body, "body");
         const response = await fetch(
-            // `${process.env.NILAI_API_URL}/v1/chat/completions`,
             `http://127.0.0.1:5000/ask_ai`,
             {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    Authorization: `Bearer ${process.env.NILAI_API_KEY}`,
                 },
                 body: JSON.stringify({
                     // model: "meta-llama/Llama-3.1-8B-Instruct",
