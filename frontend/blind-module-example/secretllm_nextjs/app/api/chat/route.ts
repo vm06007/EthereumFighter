1e8d0547fc03d900647f82e85ae1f9e9ef4f266f
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-04-04 22:05:18 +0800

    route to query local LLM endpoint

--- frontend/blind-module-example/secretllm_nextjs/app/api/chat/route.ts
+++ frontend/blind-module-example/secretllm_nextjs/app/api/chat/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     try {
         const body = await req.json();
-
+        console.log(body, "body");
         const response = await fetch(
-            `${process.env.NILAI_API_URL}/v1/chat/completions`,
+            // `${process.env.NILAI_API_URL}/v1/chat/completions`,
+            `http://127.0.0.1:5000/ask_ai`,
             {
                 method: "POST",
                 headers: {
@@ -13,8 +14,13 @@ export async function POST(req: Request) {
                     Authorization: `Bearer ${process.env.NILAI_API_KEY}`,
                 },
                 body: JSON.stringify({
-                    model: "meta-llama/Llama-3.1-8B-Instruct",
-                    messages: body.messages,
+                    // model: "meta-llama/Llama-3.1-8B-Instruct",
+                    model: "",
+                    // extract last message from the array
+                    // messages: body.messages.slice(-1),
+                    // question: "hello",
+                    question: body.messages.slice(-1)[0].content,
+                    // messages: body.messages,
                     temperature: 0.2,
                 }),
             }
