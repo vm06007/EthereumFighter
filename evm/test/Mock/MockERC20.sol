39af966884767ecdb4a20266c24e2b265f10612f
emanherawy <eman.herawy@gmaail.com>
2025-04-05 05:01:23 +0200

    add mock contracts

---
+++ evm/test/Mock/MockERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "fhevm/lib/TFHE.sol";
+
+/**
+ * @title MockERC20
+ * @dev A simple ERC20 token for testing purposes
+ */
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
